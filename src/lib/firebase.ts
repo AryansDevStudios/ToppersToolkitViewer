9aad2aa7bdd7510c949ae86014cbf665847e3965
Aryan Gupta <aryan0106gupta@gmail.com>
2025-08-09 14:56:03 +0000

    actully, use const firebaseConfig = {   apiKey: "AIzaSyAf9uNN6tI1MxAIeFQ

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,12 +6,13 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAf9uNN6tI1MxAIeFQzVTotFZicVG1m4k8",
+  authDomain: "topperstoolkit-63d3a.firebaseapp.com",
+  projectId: "topperstoolkit-63d3a",
+  storageBucket: "topperstoolkit-63d3a.firebasestorage.app",
+  messagingSenderId: "979936342565",
+  appId: "1:979936342565:web:52517f920d42c8bada05cc",
+  measurementId: "G-4MR8EHPVJW"
 };
 
 // Initialize Firebase
