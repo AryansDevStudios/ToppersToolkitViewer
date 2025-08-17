8da3062e7772cea5ad8cae01dc75fb27c781b55f
Aryan Gupta <aryan0106gupta@gmail.com>
2025-08-17 11:57:22 +0000

    use SSG (getStaticProps) for pdf viewer

--- next.config.ts
+++ next.config.ts
@@ -25,6 +25,19 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/PDFviewer/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig;
