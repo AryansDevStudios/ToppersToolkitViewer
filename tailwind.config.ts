2acded792ed6ff234e090c2158f89cac66c9fcd5
Aryan Gupta <aryan0106gupta@gmail.com>
2025-09-08 13:38:44 +0000

    But sometime it appears and sometimes the gradients do not appear Even i

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,7 @@ export default {
     extend: {
       fontFamily: {
         sans: ['var(--font-inter)', 'sans-serif'],
+        headline: ['var(--font-inter)', 'sans-serif'],
       },
       colors: {
         background: 'hsl(var(--background))',
