bef429689fba88500f8aaa042e81942ab01112fc
Aryan Gupta <aryan0106gupta@gmail.com>
2025-09-26 17:51:51 +0000

    If any user found Same gpu with another user flag them as suspect, As is

--- src/lib/iconMap.ts
+++ src/lib/iconMap.ts
@@ -1,4 +1,4 @@
-import { Atom, Dna, FlaskConical, Sigma, BookOpen, Landmark, Scale, Globe, Book, Library, Folder, FileText, Calculator, PenSquare, Code, Palette, Music, BrainCircuit, Swords, Puzzle, ClipboardList, MessageSquare, Users, Bot, Send, FileQuestion, BookCheck, Menu, Gavel, Gift, HelpCircle, History, LogOut, Trophy, Home, BookUser, Printer, ShoppingCart, Settings, UserPlus, Copy, ClipboardCheck, User } from "lucide-react";
+import { Atom, Dna, FlaskConical, Sigma, BookOpen, Landmark, Scale, Globe, Book, Library, Folder, FileText, Calculator, PenSquare, Code, Palette, Music, BrainCircuit, Swords, Puzzle, ClipboardList, MessageSquare, Users, Bot, Send, FileQuestion, BookCheck, Menu, Gavel, Gift, HelpCircle, History, LogOut, Trophy, Home, BookUser, Printer, ShoppingCart, Settings, UserPlus, Copy, ClipboardCheck, User, ShieldAlert } from "lucide-react";
 
 export const iconMap: { [key: string]: React.FC<any> } = {
   Atom,
@@ -44,6 +44,7 @@ export const iconMap: { [key: string]: React.FC<any> } = {
   Copy,
   ClipboardCheck,
   User,
+  ShieldAlert,
   BookCopy: BookOpen
 };
 
