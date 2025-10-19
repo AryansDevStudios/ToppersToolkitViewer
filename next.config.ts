e4e3344751e710fa540580489eed51a89f4f6e08
Aryan Gupta <aryan0106gupta@gmail.com>
2025-10-19 12:41:10 +0000

    set caching to 1 month

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    minimumCacheTTL: 10,
+    minimumCacheTTL: 2592000,
     remotePatterns: [
       {
         protocol: 'https',
